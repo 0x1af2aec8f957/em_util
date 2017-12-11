dfbffab6c777ceb09805126560156c5fe4e38803
Ed Me <603803799@qq.com>
2017-12-10 21:32:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ Object.assign(Array.prototype, {
 
 Object.assign(Number.prototype, {
   isNull() {
-    return !this.toString().length
+    return ![...this].length
   },
   getTime() { // 时间戳转时间[单位:s]
     const date = new Date(this);
