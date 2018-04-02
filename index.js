a3c5b88cda2fcc2683a1ccac292accef04f9039e
Ed Me <603803799@qq.com>
2018-04-02 13:01:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -253,7 +253,17 @@ export default Object.assign({
   }
  ,
  _delCookie (c_name) { // 删除cookie
-        const exp = new Date(), cval = this._getCookie(c_name)
+        const exp = new Date()
+        let cval = null
+        if (!!document.cookie.length) {
+          let c_start = document.cookie.indexOf(c_name + '=')
+          if (!!~c_start) {
+            c_start = c_start + c_name.length + 1
+            let c_end = document.cookie.indexOf(';', c_start)
+            if (!~c_end) c_end = document.cookie.length
+            cval = unescape(document.cookie.substring(c_start, c_end))
+          }
+        }
         exp.setTime(exp.getTime() - 1) // 时间月份-1
         return cval && (document.cookie = name + '=' + cval + ';expires=' + exp.toUTCString())
   }
